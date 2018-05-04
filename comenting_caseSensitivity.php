375fe7592dbb91936d2b6b90c1d218f28d2424c0
Ian Stewart <iacostewart@msn.com>
2018-05-03 21:08:20 -0400

    comments and casesensitivity

---
+++ comenting_caseSensitivity.php
@@ -0,0 +1,38 @@
+<?php
+// two forward slashes
+/*
+C-style comment block  
+*/
+
+# Shell Style Comment similar to the // 
+
+# all variables are Case Sensitive 
+$caseSensitive = "i am different";
+$CaseSensitive = "i am different";
+
+// classes are NOT case sensitive 
+/*
+class Company {
+    .......
+}
+ 
+$c1 =  new ComPanY;
+$c2 = new COMPANY;
+
+these wil not error.. 
+
+*/
+
+// functions are also not case sensitive. 
+/* 
+function companyName(){
+    ....
+}
+
+companyname();
+CompanyName();
+COMPANYNAME(); 
+these will all run as fuctions are not case sensitive in php. 
+
+*/ 
+
